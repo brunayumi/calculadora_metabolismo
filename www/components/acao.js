b0192519549f63830192419b7a19e2c35d425982
brunayumi <bruuhna.84@gmail.com>
2019-08-23 02:50:36 +0900

    alteração do java

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,3 +1,4 @@
+$(document).on("click", "#calcular", function(){
 
 var tmb;
 var peso = $("#peso").val();
@@ -6,29 +7,11 @@ var altura = $("#altura").val();
 var genero = $("#genero").val();
 var ativfisica = $("#ativfisica").val();
 
-$(document).on("click", "#calcular", function(){
-
-  if ("#genero" == "m") {
-    tmb = 66 + (13.8 * peso) + (5 * altura) - (6.8 * idade) 
+  if (genero == "m") {
+    tmb = ativfisica * (66 + (13.8 * peso) + (5 * altura) - (6.8 * idade)) 
   }
   else {
-    tmb = 655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade) 
-  }
-
-  if ("#ativfisica" == "1.2") {
-    (tmb * 1.2)
-  }
-   else if ("#ativfisica" == "1.375") {
-    (tmb * 1.375)
-  }
-  else if ("#ativfisica" == "1.55") {
-    (tmb * 1.55)
-  }
-  else if ("#ativfisica" == "1.725") {
-    (tmb * 1.725)
-  }
-  else {
-    (tmb * 1.9)
+    tmb = ativfisica * (655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade))
   }
 
 $("#resultado").val(tmb); 
